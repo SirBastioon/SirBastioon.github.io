6172691c22550d55a8aea18d074c4603344a9565
SirBastioon <sebe.wij@gmail.com>
2024-12-24 16:18:30 +0100

    Base for GitHub Pages

--- Portfolio_Page/vite.config.js
+++ Portfolio_Page/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SirBastioon.github.io/portfolio_page', // z.B. '/my-portfolio/'
+  base: 'https://github.com/SirBastioon/SirBastioon.github.io/tree/main/Portfolio_Page',
   plugins: [react()],
 });
